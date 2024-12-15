1940bce5b602585548a98d34f5d83b1bc36a2fe6
fabianmontes <fabianmontes11fab@gmail.com>
2024-12-14 18:58:09 -0600

    agregar template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> pii;
+
+#define forab(i,a,b) for(int i=a; i<=b; i++)
+#define forn(i,n) for(int i=0; i<n; i++)
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define S second
+#define F first
+
+void solve() {
+
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout<<fixed<<setprecision(5); // config. cout flotante
+	int t=1; //cin>>t;
+	while(t--) solve();
+	return 0;
+}
