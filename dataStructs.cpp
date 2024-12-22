dc370136917e596fddf89ca423ecb3fb7dfcc16a
fabianmontes <fabianmontes11fab@gmail.com>
2024-12-22 11:30:09 -0600

    cambiar nombre builtinDS -> dataStructs
    agregar sparse table y union find

--- dataStructs.cpp
+++ dataStructs.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// codigo para arbol de PBDS
+/* codigo para arbol de PBDS */
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <functional>
@@ -18,7 +18,7 @@ ordered_set mySet; // declarar objeto
 // mySet.order_of_key(x); // cuenta el numero de elementos menores a x
 // mySet.find_by_order(x); // devuelve iterador al valor conatando desde 0
 
-// operaciones de bits
+/* OPERACIONES DE BITS */
 #pragma GCC target("popcnt") // bitset con int bitwise (faster)
 // donde s es un arreglo de bits e i un iterador entero
 // s&(1<<i) // condicion i es verdadero
@@ -35,6 +35,51 @@ ordered_set mySet; // declarar objeto
 // para ll -> builtin_*ll()
 // int lsb=((s)&(-s)); // bit menos significativo 
 // cout<<bitset<32>(s)<<endl; // imprimir bits
+
+/* UNION FIND */
+struct UnionFind {
+	vector<int> p, sz, rank;
+	int numSets;
+	UnionFind(int n) : numSets(n) {
+		p.assign(n,0); for(int i=0; i<n; i++) p[i]=i;
+		rank.assign(n,0); sz.assign(n,1);
+	}
+
+	int find(int x) { while(x != p[x]) x = p[x]; return x; }
+	bool isSame(int x, int y) { return find(x) == find(y); }
+	int setSize(int x) { return sz[find(x)]; }
+
+	void join(int x, int y) {
+		if(isSame(x, y)) return;
+		int i = find(x), j = find(y);
+		if(rank[i] > rank[j]) swap(i, j);
+		if(rank[i] == rank[j]) rank[j]++;
+		sz[j] += sz[i];
+		p[i] = j;
+		numSets--;
+	}
+};
+
+/* SPARSE TABLE */
+template <class T>
+struct SparseTable {
+	vector<vector<T>> st;
+	vector<T> A;
+
+	SparseTable(vector<T>& a) : A(a) {
+		int N=a.size(), K=log2_floor(N);
+		st.resize(K+1, vector<T>(N)); st[0]=A;
+		for(int i=1; i<=K; i++)
+			for(int j=0; j+(1 << i) <= N; j++)
+				st[i][j] = min(st[i-1][j], st[i-1][j + (1<<(i-1))]);
+	}
+	T query(int l, int r) {
+		int i = log2_floor(r - l + 1);
+		return min(st[i][l], st[i][r - (1 << i) + 1]);
+	}
+
+	int log2_floor(int i) { return i ? __builtin_clz(1) - __builtin_clz(i) : -1; }
+};
 
 int main() {
 	return 0;
